efd4cf8102d1a679015cbc071e5262d945fc3250
ritaamaralsilva <rita.asilva93@gmail.com>
2026-07-06 10:25:36 +0100

    build: create yml file for github actions to build the backend

--- startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Instalar dependências da pasta backend
-pip install -r backend/requirements.txt
-
-# Iniciar o servidor
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker backend.main:app
